f32d0154104ff42eaed483d1bc917db3d270bfd3
Zhigunlimon <deadman144@gmail.com>
2018-06-16 00:54:45 +0300

    small responsive fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,9 +8,9 @@ module ApplicationHelper
 
 
   def instagram_feed_mobile
-    images = InstagramService.new.fetch.first(5)
+    images = InstagramService.new.fetch.first(1)
     images.map.with_index do |i, index|
-      content_tag(:div, '', class: "instagram_photo _#{index} col-xs-2", style: "background-image: url(#{i['url']})")
+      content_tag(:div, '', class: "instagram_photo _#{index} col-xs-12", style: "background-image: url(#{i['url']})")
     end.join.html_safe
   end
 
